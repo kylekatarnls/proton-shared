d2929d61b172f2aa725b089e06b49241a6c563bc
Richard <richard@protonmail.com>
2020-05-14 11:35:07 +0200

    Continue

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,7 +18,8 @@ export const MAX_ADDRESS_ADDON = 10;
 export const MAX_SPACE_ADDON = 16;
 
 export const DOMAIN_PLACEHOLDER = 'domain.com';
-export const EXTERNAL_EMAIL_PLACEHOLDER = 'john.doe@domain.com';
+export const EMAIL_PLACEHOLDER = 'john.doe@domain.com';
+export const USERNAME_PLACEHOLDER = 'john.doe';
 
 export enum APPS {
     PROTONMAIL = 'proton-mail',
